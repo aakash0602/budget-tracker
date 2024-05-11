59b1575257e384364fdf84960d3819ac8ec74855
Aakash <aakash@zeed.co.in>
2024-05-11 16:30:29 +0530

    wizard mutation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,7 +66,7 @@ function DesktopNavbar() {
   return (
     <div className="hidden border-separate border-b bg-background md:block">
       <nav className="container flex items-center justify-between px-8">
-        <div className="h-[80px] min-h-[60px] items-center gap-x-4">
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
           <Logo />
           <div className="flex h-full">
             {items.map((item) => (

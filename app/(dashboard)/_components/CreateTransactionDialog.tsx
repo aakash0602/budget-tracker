8cea295507d6d1a009082378f831e3ed8f5c315f
Aakash <aakash@zeed.co.in>
2024-05-14 23:21:44 +0530

    dashboard page

---
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import { CreateTransactionSchema } from "@/schema/transcation";
+import { ReactNode } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+interface Props {
+  trigger: ReactNode;
+  type: TransactionType;
+}
+
+function CreateTransactionDialog({ trigger, type }: Props) {
+  const form = useForm<CreateTransactionSchemaType>({
+    resolver: zodResolver(CreateTransactionSchema),
+    defaultValues: {
+      type,
+      date: new Date(),
+    },
+  });
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create a new{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-red-500",
+              )}
+            >
+              {type}
+            </span>
+            transaction
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={""} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transcation description (optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={0} type="number" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transcation amoount (required)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateTransactionDialog;

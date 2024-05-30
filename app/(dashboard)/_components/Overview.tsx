e1106d65c8145c25108a6e051434fdf60b977824
Aakash <aakash@zeed.co.in>
2024-05-30 23:31:35 +0530

    categories section

--- app/(dashboard)/_components/Overview.tsx
+++ app/(dashboard)/_components/Overview.tsx
@@ -7,6 +7,7 @@ import { differenceInDays, startOfMonth } from "date-fns";
 import { useState } from "react";
 import { toast } from "sonner";
 import StatsCards from "./StatsCards";
+import { CategoriesStats } from "./CategoriesStats";
 
 export function Overview({ userSettings }: { userSettings: UserSettings }) {
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
@@ -40,6 +41,12 @@ export function Overview({ userSettings }: { userSettings: UserSettings }) {
       </div>
       <div className="container w-full flex flex-col gap-2">
         <StatsCards
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
+
+        <CategoriesStats
           userSettings={userSettings}
           from={dateRange.from}
           to={dateRange.to}
